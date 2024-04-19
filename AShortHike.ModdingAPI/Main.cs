e43fd5429422fb55f6871cad3b626cd8b50f5cf4
unknown <brandenek03912@hotmail.com>
2024-04-19 10:53:28 -0400

    init: add base project files

---
+++ AShortHike.ModdingAPI/Main.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace AShortHike.ModdingAPI;
+
+[BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+[BepInDependency("AShortHike.ModdingAPI", "0.1.0")]
+public class Main : BaseUnityPlugin
+{
+    public static ModdingAPI ModdingAPI { get; private set; }
+
+    private void Awake()
+    {
+        ModdingAPI = new ModdingAPI();
+    }
+}
---
+++ AShortHike.ModdingAPI/ModdingAPI.cs
@@ -0,0 +1,13 @@
+﻿using AShortHike.ModdingAPI;
+
+namespace AShortHike.ModdingAPI;
+
+public class ModdingAPI : ShortHikeMod
+{
+    public ModdingAPI() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+    protected override void OnInitialize()
+    {
+        LogError($"{ModInfo.MOD_NAME} has been initialized");
+    }
+}
