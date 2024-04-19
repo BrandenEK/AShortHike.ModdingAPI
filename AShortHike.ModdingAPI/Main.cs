b8357a1dcd1f2592cd18cfa740518c8c0f55b65c
unknown <brandenek03912@hotmail.com>
2024-04-19 10:57:11 -0400

    core: implement proj file

--- AShortHike.ModdingAPI/Main.cs
+++ AShortHike.ModdingAPI/Main.cs
@@ -3,7 +3,6 @@
 namespace AShortHike.ModdingAPI;
 
 [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
-[BepInDependency("AShortHike.ModdingAPI", "0.1.0")]
 public class Main : BaseUnityPlugin
 {
     public static ModdingAPI ModdingAPI { get; private set; }
