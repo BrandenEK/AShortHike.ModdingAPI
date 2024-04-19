2a279040eecf4d9df41b1713aecf739beb6401b2
unknown <brandenek03912@hotmail.com>
2024-04-19 15:41:34 -0400

    core: add shorthikemod

--- AShortHike.ModdingAPI/ModdingAPI.cs
+++ AShortHike.ModdingAPI/ModdingAPI.cs
@@ -1,12 +1,11 @@
-﻿using AShortHike.ModdingAPI;
-
+﻿
 namespace AShortHike.ModdingAPI;
 
 public class ModdingAPI : ShortHikeMod
 {
     public ModdingAPI() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
 
-    protected override void OnInitialize()
+    protected internal override void OnInitialize()
     {
         LogError($"{ModInfo.MOD_NAME} has been initialized");
     }
---
+++ AShortHike.ModdingAPI/ShortHikeMod.cs
@@ -0,0 +1,131 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+
+namespace AShortHike.ModdingAPI;
+
+public abstract class ShortHikeMod
+{
+    // Mod info
+
+    /// <summary>
+    /// The unique id of the mod
+    /// </summary>
+    public string Id => id;
+    private readonly string id;
+
+    /// <summary>
+    /// The display name of the mod
+    /// </summary>
+    public string Name => name;
+    private readonly string name;
+
+    /// <summary>
+    /// The developer of the mod
+    /// </summary>
+    public string Author => author;
+    private readonly string author;
+
+    /// <summary>
+    /// The file version of the mod
+    /// </summary>
+    public string Version => version;
+    private readonly string version;
+
+    // Events
+
+    /// <summary>
+    /// Called when starting the game, at the same time as other managers
+    /// </summary>
+    protected internal virtual void OnInitialize() { }
+
+    /// <summary>
+    /// Called when starting the game, after all other mods have been initialized
+    /// </summary>
+    protected internal virtual void OnAllInitialized() { }
+
+    /// <summary>
+    /// Called when exiting the game, at the same time as other managers
+    /// </summary>
+    protected internal virtual void OnDispose() { }
+
+    /// <summary>
+    /// Called every frame after initialization
+    /// </summary>
+    protected internal virtual void OnUpdate() { }
+
+    /// <summary>
+    /// Called at the end of every frame after initialization
+    /// </summary>
+    protected internal virtual void OnLateUpdate() { }
+
+    /// <summary>
+    /// Called when a new level is loaded, including the main menu
+    /// </summary>
+    protected internal virtual void OnLevelLoaded(string oldLevel, string newLevel) { }
+
+    /// <summary>
+    /// Called when an old level is unloaded, including the main menu
+    /// </summary>
+    protected internal virtual void OnLevelUnloaded(string oldLevel, string newLevel) { }
+
+    /// <summary>
+    /// Called when starting a new game on the main menu, after data is reset
+    /// </summary>
+    protected internal virtual void OnNewGame() { }
+
+    /// <summary>
+    /// Called when loading an existing game on the main menu, after data is reset
+    /// </summary>
+    protected internal virtual void OnLoadGame() { }
+
+    /// <summary>
+    /// Called when quiting a game, after returning to the main menu
+    /// </summary>
+    protected internal virtual void OnExitGame() { }
+
+    // Logging
+
+    /// <summary>
+    /// Logs a message in white to the console
+    /// </summary>
+    public void Log(object message) => _logger.LogMessage(message);
+
+    /// <summary>
+    /// Logs a message in yellow to the console
+    /// </summary>
+    public void LogWarning(object warning) => _logger.LogWarning(warning);
+
+    /// <summary>
+    /// Logs a message in red to the console
+    /// </summary>
+    public void LogError(object error) => _logger.LogError(error);
+
+    // Constructor
+
+    /// <summary>
+    /// Initializes and registers a new BlasII mod
+    /// </summary>
+    public ShortHikeMod(string id, string name, string author, string version)
+    {
+        // Set data
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.version = version;
+
+        // Set handlers
+        //_configHandler = new ConfigHandler(this);
+        //_fileHandler = new FileHandler(this);
+        //_inputHandler = new InputHandler(this);
+        //_localizationHandler = new LocalizationHandler(this);
+
+        // Register and patch mod
+        //if (Main.ModLoader.RegisterMod(this))
+        //{
+            new Harmony(id).PatchAll(GetType().Assembly);
+            _logger = Logger.CreateLogSource(name);
+        //}
+    }
+
+    private readonly ManualLogSource _logger;
+}
